316c11d8834f7f05b7a03a6e92479a87534506ea
Raquel Cardoso <raquel.cardoso@toonelada.pt>
2023-09-22 02:46:24 +0100

    more fixes for a.1 and a.2

--- run.sh
+++ run.sh
@@ -12,16 +12,17 @@ done
 
 # ############ CORE OF THE PROJECT  ############
 
-fstconcat compiled/aux_day.fst compiled/aux_slash.fst | fstrmepsilon | fstarcsort > compiled/tmp_day.fst
-fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait compiled/tmp_day.fst | dot -Tpdf > images/tmp_day.pdf
+fstconcat compiled/aux_day.fst compiled/aux_slash.fst | fstrmepsilon | fstarcsort > compiled/tmp_day.fst # dia/
+#fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait compiled/tmp_day.fst | dot -Tpdf > images/tmp_day.pdf
+fstconcat compiled/mmm2mm.fst compiled/aux_slash.fst | fstrmepsilon | fstarcsort > compiled/tmp_month.fst # mes/
 
-fstconcat compiled/mmm2mm.fst compiled/aux_slash.fst | fstrmepsilon | fstarcsort > compiled/tmp_month.fst
-fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait compiled/tmp_month.fst | dot -Tpdf > images/tmp_month.pdf
+#fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait compiled/tmp_month.fst | dot -Tpdf > images/tmp_month.pdf
 
-fstconcat compiled/tmp_month.fst compiled/tmp_day.fst | fstrmepsilon | fstarcsort > compiled/tmp_monthday.fst
-fstconcat compiled/tmp_monthday.fst compiled/aux_year.fst | fstrmepsilon | fstarcsort > compiled/mix2numerical.fst
 
-fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait compiled/mix2numerical.fst | dot -Tpdf > images/mix2numerical.pdf
+fstconcat compiled/tmp_month.fst compiled/tmp_day.fst | fstrmepsilon | fstarcsort > compiled/tmp_monthday.fst # mes/dia/
+fstconcat compiled/tmp_monthday.fst compiled/aux_year.fst | fstrmepsilon | fstarcsort > compiled/mix2numerical.fst # mes/dia/ano
+
+#fstdraw --isymbols=syms.txt --osymbols=syms.txt --portrait compiled/mix2numerical.fst | dot -Tpdf > images/mix2numerical.pdf
 
 
 # ############ generate PDFs  ############
