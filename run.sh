52db9eb49756c27daeff714c01d4cba1f4266d39
Raquel Cardoso <raquel.cardoso@toonelada.pt>
2023-10-01 17:09:41 +0100

    removing unnecessary files and adding comments

--- run.sh
+++ run.sh
@@ -11,7 +11,7 @@ done
 
 # ############ CORE OF THE PROJECT  ############
 
-# 2
+# TRANSDUCER 2 - mix2numerical.fst
 
 fstconcat compiled/aux_day.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_day.fst
 fstconcat compiled/mmm2mm.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_month.fst
@@ -19,14 +19,14 @@ fstconcat compiled/mmm2mm.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp
 fstconcat compiled/tmp_month.fst compiled/tmp_day.fst | fstarcsort > compiled/tmp_monthday.fst
 fstconcat compiled/tmp_monthday.fst compiled/aux_year.fst | fstarcsort > compiled/mix2numerical.fst
 
-# 3
+# TRANSDUCER 3 - pt2en.fst
 
 fstconcat compiled/month_pt2en.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_pt2en1.fst
 fstconcat compiled/aux_day.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_pt2en2.fst
 fstconcat compiled/tmp_pt2en1.fst compiled/tmp_pt2en2.fst | fstarcsort > compiled/tmp_pt2en3.fst
 fstconcat compiled/tmp_pt2en3.fst compiled/aux_year.fst | fstarcsort > compiled/pt2en.fst
 
-# 4
+# TRANSDUCER 4 - en2pt.fst
 
 fstinvert compiled/month_pt2en.fst > compiled/month_en2pt.fst
 
@@ -35,7 +35,7 @@ fstconcat compiled/aux_day.fst compiled/aux_slash.fst | fstarcsort > compiled/tm
 fstconcat compiled/tmp_en2pt1.fst compiled/tmp_en2pt2.fst | fstarcsort > compiled/tmp_en2pt3.fst
 fstconcat compiled/tmp_en2pt3.fst compiled/aux_year.fst | fstarcsort > compiled/en2pt.fst
 
-#8
+# TRANSDUCER 8 - datenum2text.fst
 
 fstconcat compiled/month.fst compiled/aux_slash2.fst | fstarcsort > compiled/tmp_month.fst
 fstconcat compiled/day.fst compiled/aux_slash3.fst | fstarcsort > compiled/tmp_day.fst
@@ -43,7 +43,7 @@ fstconcat compiled/day.fst compiled/aux_slash3.fst | fstarcsort > compiled/tmp_d
 fstconcat compiled/tmp_month.fst compiled/tmp_day.fst | fstarcsort > compiled/tmp_monthday.fst
 fstconcat compiled/tmp_monthday.fst compiled/year.fst | fstarcsort > compiled/datenum2text.fst
 
-#9 
+# TRANSDUCER 9 - mix2text.fst
 
 #accepting portuguese mixed format and outputing english text format
 fstcompose compiled/pt2en.fst compiled/mix2numerical.fst > compiled/pt2en_mix2num.fst
@@ -55,9 +55,13 @@ fstcompose compiled/mix2numerical.fst compiled/datenum2text.fst > compiled/en_mi
 #accepting portuguese and english mixed format and outputing english text format
 fstunion compiled/pt2en_mix2text.fst compiled/en_mix2text.fst > compiled/mix2text.fst
 
-#10
+# TRANSDUCER 10 - date2text.fst
 
 fstunion compiled/mix2text.fst compiled/datenum2text.fst > compiled/date2text.fst
+
+
+# ############ END OF CORE OF THE PROJECT  ############
+
 
 # ############ generate PDFs  ############
 echo "Starting to generate PDFs"
@@ -66,17 +70,17 @@ for i in compiled/*.fst; do
    fstdraw --portrait --isymbols=syms.txt --osymbols=syms.txt $i | dot -Tpdf > images/$(basename $i '.fst').pdf
 done
 
-# TESTING
+
+# ############ TESTING THE PROJECT  ############
 echo " "
 
-
-#1 - generates files
+# method 1 - generates files
 echo "TESTING - METHOD 1"
 echo " "
 
-echo "***********************************************************"
+echo "***************************************************************"
 echo "Testing date2text.fst (the output is a transducer: fst and pdf)"
-echo "***********************************************************"
+echo "***************************************************************"
 for w in compiled/t-*.fst; do
     fstcompose $w compiled/date2text.fst | fstshortestpath | fstproject --project_type=output |
                   fstrmepsilon | fsttopsort > compiled/$(basename $w ".fst")-out.fst
@@ -88,7 +92,7 @@ for i in compiled/t-*-out.fst; do
 done
 
 
-#3 - presents the output with the tokens concatenated (uses a different syms on the output)
+# method 3 - presents the output with the tokens concatenated (uses a different syms on the output)
 echo "TESTING - METHOD 3"
 echo " "
 
@@ -97,9 +101,9 @@ fst2word() {
 }
 
 trans=mix2numerical.fst
-echo "***********************************************************"
+echo "****************************************************************"
 echo "Testing mix2numerical  (output is a string  using 'syms-out.txt')"
-echo "***********************************************************"
+echo "****************************************************************"
 for w in "NOV/19/2020" "JUN/28/2020"; do
     res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                        fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
@@ -109,9 +113,9 @@ for w in "NOV/19/2020" "JUN/28/2020"; do
 done
 
 trans=en2pt.fst
-echo "***********************************************************"
+echo "********************************************************"
 echo "Testing en2pt (output is a string  using 'syms-out.txt')"
-echo "***********************************************************"
+echo "********************************************************"
 for w in "NOV/19/2020" "JUN/28/2020"; do
     res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                        fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
@@ -121,9 +125,9 @@ for w in "NOV/19/2020" "JUN/28/2020"; do
 done
 
 trans=datenum2text.fst
-echo "***********************************************************"
+echo "***************************************************************"
 echo "Testing datenum2text (output is a string  using 'syms-out.txt')"
-echo "***********************************************************"
+echo "***************************************************************"
 for w in "11/19/2020" "06/28/2020" "6/28/2020" "4/3/2001" "04/3/2001" "04/03/2001"; do
     res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                        fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
@@ -145,9 +149,9 @@ for w in "NOV/19/2020" "JUN/28/2020" "ABR/3/2001" "APR/03/2001"; do
 done
 
 trans=date2text.fst
-echo "***********************************************************"
+echo "************************************************************"
 echo "Testing date2text (output is a string  using 'syms-out.txt')"
-echo "***********************************************************"
+echo "************************************************************"
 for w in "NOV/19/2020" "JUN/28/2020" "ABR/3/2001" "APR/03/2001" "11/19/2020"; do
     res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                        fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
