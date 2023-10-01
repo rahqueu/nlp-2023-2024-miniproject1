df1079f26b76104c8aa0ddccfeae65f64512885e
Raquel Cardoso <raquel.cardoso@toonelada.pt>
2023-10-01 16:18:49 +0100

    project base is done

--- run.sh
+++ run.sh
@@ -13,11 +13,11 @@ done
 
 # 2
 
-fstconcat compiled/aux_day.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_day.fst # dia + / = dia/
-fstconcat compiled/mmm2mm.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_month.fst # mes + / = mes/
+fstconcat compiled/aux_day.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_day.fst
+fstconcat compiled/mmm2mm.fst compiled/aux_slash.fst | fstarcsort > compiled/tmp_month.fst
 
-fstconcat compiled/tmp_month.fst compiled/tmp_day.fst | fstarcsort > compiled/tmp_monthday.fst # mes/ + dia/ = mes/dia/
-fstconcat compiled/tmp_monthday.fst compiled/aux_year.fst | fstarcsort > compiled/mix2numerical.fst # mes/dia/ + ano = mes/dia/ano
+fstconcat compiled/tmp_month.fst compiled/tmp_day.fst | fstarcsort > compiled/tmp_monthday.fst
+fstconcat compiled/tmp_monthday.fst compiled/aux_year.fst | fstarcsort > compiled/mix2numerical.fst
 
 # 3
 
@@ -45,9 +45,19 @@ fstconcat compiled/tmp_monthday.fst compiled/year.fst | fstarcsort > compiled/da
 
 #9 
 
-fstunion compiled/en2pt.fst compiled/pt2en.fst > compiled/enptaux.fst
-fstcompose compiled/enptaux.fst compiled/mix2numerical.fst > compiled/mixaux.fst
-fstcompose compiled/mixaux.fst compiled/datenum2text.fst > compiled/mix2text.fst
+#accepting portuguese mixed format and outputing english text format
+fstcompose compiled/pt2en.fst compiled/mix2numerical.fst > compiled/pt2en_mix2num.fst
+fstcompose compiled/pt2en_mix2num.fst compiled/datenum2text.fst > compiled/pt2en_mix2text.fst
+
+#accepting english mixed format and outputing english text format
+fstcompose compiled/mix2numerical.fst compiled/datenum2text.fst > compiled/en_mix2text.fst
+
+#accepting portuguese and english mixed format and outputing english text format
+fstunion compiled/pt2en_mix2text.fst compiled/en_mix2text.fst > compiled/mix2text.fst
+
+#10
+
+fstunion compiled/mix2text.fst compiled/datenum2text.fst > compiled/date2text.fst
 
 # ############ generate PDFs  ############
 echo "Starting to generate PDFs"
@@ -65,10 +75,10 @@ echo "TESTING - METHOD 1"
 echo " "
 
 echo "***********************************************************"
-echo "Testing mix2text.fst (the output is a transducer: fst and pdf)"
+echo "Testing date2text.fst (the output is a transducer: fst and pdf)"
 echo "***********************************************************"
-for w in compiled/t-*_mix.fst; do
-    fstcompose $w compiled/mix2text.fst | fstshortestpath | fstproject --project_type=output |
+for w in compiled/t-*.fst; do
+    fstcompose $w compiled/date2text.fst | fstshortestpath | fstproject --project_type=output |
                   fstrmepsilon | fsttopsort > compiled/$(basename $w ".fst")-out.fst
 done
 for i in compiled/t-*-out.fst; do
@@ -126,7 +136,19 @@ trans=mix2text.fst
 echo "***********************************************************"
 echo "Testing mix2text (output is a string  using 'syms-out.txt')"
 echo "***********************************************************"
-for w in "NOV/19/2020" "JUN/28/2020" "ABR/3/2001" "ABR/03/2001"; do
+for w in "NOV/19/2020" "JUN/28/2020" "ABR/3/2001" "APR/03/2001"; do
+    res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
+                       fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
+                       fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
+    echo "$w = $res"
+    echo " "
+done
+
+trans=date2text.fst
+echo "***********************************************************"
+echo "Testing date2text (output is a string  using 'syms-out.txt')"
+echo "***********************************************************"
+for w in "NOV/19/2020" "JUN/28/2020" "ABR/3/2001" "APR/03/2001" "11/19/2020"; do
     res=$(python3 ./scripts/word2fst.py $w | fstcompile --isymbols=syms.txt --osymbols=syms.txt | fstarcsort |
                        fstcompose - compiled/$trans | fstshortestpath | fstproject --project_type=output |
                        fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=./scripts/syms-out.txt | fst2word)
